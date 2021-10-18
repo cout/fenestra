dfa61df8996a9a36506276b1b91554a1ee7ca90b
Paul Brannan <cout@users.noreply.github.com>
2021-10-18 11:44:32 -0400

    Add major/minor faults

--- plugins/Rusage.hpp
+++ plugins/Rusage.hpp
@@ -25,16 +25,20 @@ public:
       std::cout << "getrusage failed" << std::endl;
     }
 
-    if (!cs_key_) {
-      cs_key_ = dictionary["Context switches"];
-    }
+    if (!cs_key_) { cs_key_ = dictionary["Context switches"]; }
+    if (!minflt_key_) { minflt_key_ = dictionary["Minor faults"]; }
+    if (!majflt_key_) { majflt_key_ = dictionary["Major faults"]; }
 
     bool primed = updates_ > 2;
     auto nvcsw = usage.ru_nvcsw - last_usage_.ru_nvcsw;
     auto nivcsw = usage.ru_nivcsw - last_usage_.ru_nivcsw;
     Probe::Value cs = primed ? nvcsw + nivcsw : 0;
+    Probe::Value minflt = usage.ru_minflt - last_usage_.ru_minflt;
+    Probe::Value majflt = usage.ru_majflt - last_usage_.ru_majflt;
 
     probe.meter(*cs_key_, Probe::VALUE, 0, cs);
+    probe.meter(*minflt_key_, Probe::VALUE, 0, minflt);
+    probe.meter(*majflt_key_, Probe::VALUE, 0, majflt);
 
     last_usage_ = usage;
     ++updates_;
@@ -42,6 +46,8 @@ public:
 
 private:
   std::optional<Probe::Key> cs_key_;
+  std::optional<Probe::Key> minflt_key_;
+  std::optional<Probe::Key> majflt_key_;
   rusage last_usage_;
   std::uint64_t updates_= 0;
 };
