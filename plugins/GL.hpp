9506c85c189706bff169e9e7c9ede4e1033ed0fa
Paul Brannan <cout@users.noreply.github.com>
2021-10-24 14:43:16 -0400

    Record render latency

--- plugins/GL.hpp
+++ plugins/GL.hpp
@@ -10,6 +10,7 @@
 #include <stdexcept>
 #include <sstream>
 #include <map>
+#include <array>
 
 namespace fenestra {
 
@@ -74,9 +75,21 @@ public:
 
     tex_w_ = geom.max_width();
     tex_h_ = geom.max_height();
+
+    glGenQueries(query_ids_.size(), query_ids_.data());
   }
 
   virtual void video_refresh(const void * data, unsigned int width, unsigned int height, std::size_t pitch) override {
+    next_query_idx_ = (query_idx_ + 1) % query_ids_.size();
+    bool do_query = (next_query_idx_ != result_idx_) && !query_started_;
+
+    if (do_query) {
+      flush_errors();
+      glBeginQuery(GL_TIME_ELAPSED, query_ids_[query_idx_]);
+      show_errors("glBeginQuery");
+      query_started_ = true;
+    }
+
     glBindTexture(GL_TEXTURE_2D, tex_id_);
     glPixelStorei(GL_UNPACK_ROW_LENGTH, pitch / (pixel_format_.bpp / CHAR_BIT));
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, pixel_format_.format, pixel_format_.type, data);
@@ -101,6 +114,46 @@ public:
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     glBindTexture(GL_TEXTURE_2D, 0);
+
+    if (query_started_) {
+      flush_errors();
+      glEndQuery(GL_TIME_ELAPSED);
+      show_errors("glEndQuery");
+      query_idx_ = next_query_idx_;
+      query_started_ = false;
+    }
+  }
+
+  virtual void collect_metrics(Probe & probe, Probe::Dictionary & dictionary) override {
+    if (!render_latency_key_) { render_latency_key_ = dictionary["Render latency (µs)"]; }
+
+    GLint available = 0;
+    glGetQueryObjectiv(query_ids_[result_idx_], GL_QUERY_RESULT_AVAILABLE, &available);
+
+    if (available) {
+      GLuint result = 0;
+      glGetQueryObjectuiv(query_ids_[result_idx_], GL_QUERY_RESULT, &result);
+
+      probe.meter(*render_latency_key_, Probe::VALUE, 0, result / 1'000);
+
+      result_idx_ = (result_idx_ + 1) % query_ids_.size();
+    } else {
+      probe.meter(*render_latency_key_, Probe::VALUE, 0, 0);
+    }
+
+    show_errors("GL");
+  }
+
+  void flush_errors() {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) { }
+  }
+
+  void show_errors(char const * op) {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+      std::cout << op << ": " << gluErrorString(err) << " (" << err << ")" << std::endl;
+    }
   }
 
 private:
@@ -111,6 +164,14 @@ private:
   GLfloat render_h_ = 0;
 
   Pixel_Format pixel_format_;
+
+  std::size_t result_idx_ = 0;
+  std::size_t query_idx_ = 0;
+  std::size_t next_query_idx_ = 0;
+  std::array<GLuint, 16> query_ids_;
+
+  std::optional<Probe::Key> render_latency_key_;
+  bool query_started_ = false;
 };
 
 }
