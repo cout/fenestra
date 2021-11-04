f96efe21f5e03896388ebc7f6623da294e67317f
Paul Brannan <cout@users.noreply.github.com>
2021-11-04 05:31:57 -0400

    Move button/axis bindings to glfw-gamepad plugin

--- Config.hpp
+++ Config.hpp
@@ -18,17 +18,6 @@
 
 namespace fenestra {
 
-struct Button_Binding {
-  unsigned int button;
-  unsigned int retro_button;
-};
-
-struct Axis_Binding {
-  unsigned int axis;
-  float threshold;
-  unsigned int retro_button;
-};
-
 struct Abstract_Setting {
   virtual ~Abstract_Setting() { }
 };
@@ -75,9 +64,6 @@ public:
 
   char const * system_directory() const { return system_directory_.c_str(); }
   char const * save_directory() const { return save_directory_.c_str(); }
-
-  auto const & button_bindings() const { return button_bindings_; }
-  auto const & axis_bindings() const { return axis_bindings_; }
 
   template <typename T, typename Default>
   T & fetch(std::string const & name, Default dflt) const {
@@ -209,29 +195,6 @@ private:
 
   std::string & system_directory_;
   std::string & save_directory_;
-
-  // Button bindings for 8bitdo SN30+
-  std::vector<Button_Binding> button_bindings_ = {
-    { 0, RETRO_DEVICE_ID_JOYPAD_B },
-    { 1, RETRO_DEVICE_ID_JOYPAD_A },
-    { 2, RETRO_DEVICE_ID_JOYPAD_Y },
-    { 3, RETRO_DEVICE_ID_JOYPAD_X },
-    { 4, RETRO_DEVICE_ID_JOYPAD_L },
-    { 5, RETRO_DEVICE_ID_JOYPAD_R },
-    { 6, RETRO_DEVICE_ID_JOYPAD_SELECT },
-    { 7, RETRO_DEVICE_ID_JOYPAD_START },
-    { 11, RETRO_DEVICE_ID_JOYPAD_UP },
-    { 12, RETRO_DEVICE_ID_JOYPAD_RIGHT },
-    { 13, RETRO_DEVICE_ID_JOYPAD_DOWN },
-    { 14, RETRO_DEVICE_ID_JOYPAD_LEFT },
-  };
-
-  // Axis bindings for 8bitdo SN30+
-  std::vector<Axis_Binding> axis_bindings_ = {
-    { 4, +1, RETRO_DEVICE_ID_JOYPAD_L2 },
-    { 5, +1, RETRO_DEVICE_ID_JOYPAD_R2 },
-};
-
 };
 
 }
--- plugins/GLFWGamepad.hpp
+++ plugins/GLFWGamepad.hpp
@@ -13,8 +13,7 @@ class GLFWGamepad
 {
 public:
   GLFWGamepad(Config const & config)
-    : config_(config)
-    , joystick_(config.fetch<unsigned int>("glfw-gamepad.joystick", 0))
+    : joystick_(config.fetch<unsigned int>("glfw-gamepad.joystick", 0))
     , port_(config.fetch<unsigned int>("glfw-gamepad.port", 0))
     , enabled_(config.fetch<bool>("glfw-gamepad.enabled", true))
   {
@@ -28,12 +27,12 @@ public:
     GLFWgamepadstate gamepad_state;
 
     if (glfwGetGamepadState(joystick_, &gamepad_state)) {
-      for (auto const & binding : config_.button_bindings()) {
+      for (auto const & binding : button_bindings_) {
         auto pressed = gamepad_state.buttons[binding.button];
         state.input_state[port_].pressed[binding.retro_button] = pressed;
       }
 
-      for (auto const & binding : config_.axis_bindings()) {
+      for (auto const & binding : axis_bindings_) {
         auto pos = gamepad_state.axes[binding.axis];
         auto thresh = binding.threshold;
         auto pressed = thresh > 0 ? pos >= thresh : pos <= thresh;
@@ -43,7 +42,39 @@ public:
   }
 
 private:
-  Config const & config_;
+  struct ButtonBinding {
+    unsigned int button;
+    unsigned int retro_button;
+  };
+
+  // Button bindings for 8bitdo SN30+
+  static constexpr inline std::array button_bindings_ = {
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_B, RETRO_DEVICE_ID_JOYPAD_B },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_A, RETRO_DEVICE_ID_JOYPAD_A },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_Y, RETRO_DEVICE_ID_JOYPAD_Y },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_X, RETRO_DEVICE_ID_JOYPAD_X },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_LEFT_BUMPER, RETRO_DEVICE_ID_JOYPAD_L },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, RETRO_DEVICE_ID_JOYPAD_R },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_GUIDE, RETRO_DEVICE_ID_JOYPAD_SELECT },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_START, RETRO_DEVICE_ID_JOYPAD_START },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_DPAD_UP, RETRO_DEVICE_ID_JOYPAD_UP },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_DPAD_RIGHT, RETRO_DEVICE_ID_JOYPAD_RIGHT },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_DPAD_DOWN, RETRO_DEVICE_ID_JOYPAD_DOWN },
+    ButtonBinding { GLFW_GAMEPAD_BUTTON_DPAD_LEFT, RETRO_DEVICE_ID_JOYPAD_LEFT },
+  };
+
+  struct AxisBinding {
+    unsigned int axis;
+    float threshold;
+    unsigned int retro_button;
+  };
+
+  // Axis bindings for 8bitdo SN30+
+  static constexpr inline std::array axis_bindings_ = {
+    AxisBinding { GLFW_GAMEPAD_AXIS_LEFT_TRIGGER, +1, RETRO_DEVICE_ID_JOYPAD_L2 },
+    AxisBinding { GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER, +1, RETRO_DEVICE_ID_JOYPAD_R2 },
+  };
+
   unsigned int const & joystick_;
   unsigned int const & port_;
   bool const & enabled_;
