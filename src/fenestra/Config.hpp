73f2e86d6d1d37004cee0ef3294e49c169e26027
Paul Brannan <cout@users.noreply.github.com>
2021-11-19 00:05:16 -0500

    Go back to using std::function in the config
    This slightly increases build time, but does not seem to be as bad as it
    was before.  The simplicity is worth it.

--- src/fenestra/Config.hpp
+++ src/fenestra/Config.hpp
@@ -18,62 +18,6 @@
 namespace fenestra {
 
 class Config {
-private:
-  class Setter {
-  public:
-    virtual ~Setter() { }
-
-    virtual void operator()(Json::Value const & cfg) = 0;
-
-  protected:
-    template <typename T>
-    static void log_value(std::string const & name, std::vector<T> const & value) {
-    }
-
-    template <typename T>
-    static void log_value(std::string const & name, std::set<T> const & value) {
-    }
-
-    template <typename T>
-    static void log_value(std::string const & name, std::map<std::string, T> const & value) {
-    }
-
-    static void log_value(std::string const & name, Milliseconds const & value) {
-      std::cout << "[INFO config] " << name << ": " << value.count() << std::endl;
-    }
-
-    template <typename T>
-    static void log_value(std::string const & name, T const & value) {
-      std::cout << "[INFO config] " << name << ": " << value << std::endl;
-    }
-  };
-
-  template<typename T>
-  class Setter_T
-    : public Setter
-  {
-  public:
-    Setter_T(std::string const  & name, T * value)
-      : name_(name)
-      , value_(value)
-    {
-    }
-
-    virtual void operator()(Json::Value const & cfg) override {
-      auto cfg_value = json::deep_find(&cfg, name_);
-      if (cfg_value) {
-        json::assign(*value_, *cfg_value);
-        log_value(name_, *value_);
-      }
-    }
-
-    virtual ~Setter_T() { }
-
-  private:
-    std::string name_;
-    T * value_;
-  };
-
 public:
   Config()
   {
@@ -97,8 +41,14 @@ public:
     if (it == values_.end()) {
       auto [ inserted_it, inserted ] = values_.emplace(name, std::make_any<T>(dflt));
       T * value = std::any_cast<T>(&inserted_it->second);
-      setters_.emplace_back(new Setter_T<T>(name, value));
-      (*setters_.back())(cfg_);
+      auto & setter = setters_.emplace_back([name, value](Json::Value const & cfg) {
+        auto cfg_value = json::deep_find(&cfg, name);
+        if (cfg_value) {
+          json::assign(*value, *cfg_value);
+          log_value(name, *value);
+        }
+      });
+      setter(cfg_);
       return *value;
     } else {
       return *std::any_cast<T>(&it->second);
@@ -108,14 +58,35 @@ public:
 private:
   void refresh() {
     for (auto const & setter : setters_) {
-      (*setter)(cfg_);
+      setter(cfg_);
     }
+  }
+
+  template <typename T>
+  static void log_value(std::string const & name, std::vector<T> const & value) {
+  }
+
+  template <typename T>
+  static void log_value(std::string const & name, std::set<T> const & value) {
+  }
+
+  template <typename T>
+  static void log_value(std::string const & name, std::map<std::string, T> const & value) {
+  }
+
+  static void log_value(std::string const & name, Milliseconds const & value) {
+    std::cout << "[INFO config] " << name << ": " << value.count() << std::endl;
+  }
+
+  template <typename T>
+  static void log_value(std::string const & name, T const & value) {
+    std::cout << "[INFO config] " << name << ": " << value << std::endl;
   }
 
 private:
   Json::Value cfg_;
   mutable std::map<std::string, std::any> values_;
-  mutable std::vector<std::unique_ptr<Setter>> setters_;
+  mutable std::vector<std::function<void (Json::Value const &)>> setters_;
 };
 
 }
