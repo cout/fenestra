087a0c5e611e0706e8e83831d14e9d5d0c3217db
Paul Brannan <cout@users.noreply.github.com>
2021-12-04 21:41:57 -0500

    Implement SSR capture for pixel format XRGB8888
    Implemented XRGB8888 in the SSR plugin; this is the format used by
    nestopia.

--- src/fenestra/plugins/SSR.hpp
+++ src/fenestra/plugins/SSR.hpp
@@ -19,7 +19,8 @@ public:
     retro_pixel_format format;
   };
 
-  static inline std::map<unsigned int, Pixel_Format> const pixel_formats = {
+  static inline std::map<retro_pixel_format, Pixel_Format> const pixel_formats = {
+    { RETRO_PIXEL_FORMAT_XRGB8888, { 32, RETRO_PIXEL_FORMAT_XRGB8888 } },
     { RETRO_PIXEL_FORMAT_RGB565,   { 16, RETRO_PIXEL_FORMAT_RGB565 } },
   };
 
@@ -59,8 +60,7 @@ public:
     ptr_ = ssr_->NewFrame(&flags);
 
     if (ptr_) {
-      auto Bpp = pixel_format_.bpp / CHAR_BIT;
-      auto size = Bpp * height * pitch;
+      auto size = height * pitch;
 
       if (buf_.size() != size) {
         buf_.resize(size);
@@ -85,17 +85,7 @@ public:
       for (std::size_t y = 0; y < height_; ++y) {
         auto start = buf_.data() + y * pitch_;
         auto end = start + width_ * Bpp;
-        switch (pixel_format_.format) {
-          case RETRO_PIXEL_FORMAT_RGB565:
-            dest = serialize_bgra<std::uint16_t, RGB565>(start, end, dest);
-            break;
-
-          case RETRO_PIXEL_FORMAT_0RGB1555:
-          case RETRO_PIXEL_FORMAT_XRGB8888:
-          case RETRO_PIXEL_FORMAT_UNKNOWN:
-            // not implemented
-            break;
-        }
+        dest = serialize(start, end, dest, pixel_format_.format);
       }
 
       ssr_->NextFrame();
@@ -115,6 +105,10 @@ private:
     static inline std::uint8_t bscale[256];
     static inline std::uint8_t gscale[256];
     static inline std::uint8_t rscale[256];
+
+    static inline std::uint8_t scaleb(std::uint8_t blue)  { return bscale[blue];  }
+    static inline std::uint8_t scaleg(std::uint8_t green) { return gscale[green]; }
+    static inline std::uint8_t scaler(std::uint8_t red)   { return rscale[red];   }
   };
 
   struct RGB565_Init {
@@ -129,6 +123,19 @@ private:
   };
 
   static inline RGB565_Init rgb565_init_;
+
+  struct XRGB8888 {
+    static constexpr inline auto bshift = 0;
+    static constexpr inline auto gshift = 8;
+    static constexpr inline auto rshift = 16;
+    static constexpr inline auto bmask = 0xff;
+    static constexpr inline auto gmask = 0xff;
+    static constexpr inline auto rmask = 0xff;
+
+    static inline std::uint8_t scaleb(std::uint8_t blue)  { return blue;  }
+    static inline std::uint8_t scaleg(std::uint8_t green) { return green; }
+    static inline std::uint8_t scaler(std::uint8_t red)   { return red;   }
+  };
 
   template<typename T, typename Params>
   static inline std::uint8_t * serialize_bgra(char const * start, char const * end, std::uint8_t * dest) {
@@ -147,10 +154,27 @@ private:
     auto green = (i >> Params::gshift) & Params::gmask;
     auto red   = (i >> Params::rshift) & Params::rmask;
 
-    *dest++ = Params::bscale[blue];
-    *dest++ = Params::gscale[green];
-    *dest++ = Params::rscale[red];
+    *dest++ = Params::scaleb(blue);
+    *dest++ = Params::scaleg(green);
+    *dest++ = Params::scaler(red);
     *dest++ = 255;
+
+    return dest;
+  }
+
+  static inline std::uint8_t * serialize(char const * start, char const * end, std::uint8_t * dest, retro_pixel_format format) {
+    switch (format) {
+      case RETRO_PIXEL_FORMAT_RGB565:
+        return serialize_bgra<std::uint16_t, RGB565>(start, end, dest);
+
+      case RETRO_PIXEL_FORMAT_XRGB8888:
+        return serialize_bgra<std::uint32_t, XRGB8888>(start, end, dest);
+
+      case RETRO_PIXEL_FORMAT_0RGB1555:
+      case RETRO_PIXEL_FORMAT_UNKNOWN:
+        // not implemented
+        break;
+    }
 
     return dest;
   }
