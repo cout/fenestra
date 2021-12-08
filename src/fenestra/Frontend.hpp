1eb6fd056bc044c10c16b9147ddc5c2f018ea101
Paul Brannan <cout@users.noreply.github.com>
2021-12-07 22:34:43 -0500

    Identify plugin instances with their full name instead of separate name/instance

--- src/fenestra/Frontend.hpp
+++ src/fenestra/Frontend.hpp
@@ -63,18 +63,17 @@ public:
       auto idx = configured_name_instance.find(":");
       if (idx != std::string::npos) {
         auto configured_name = configured_name_instance.substr(0, idx);
-        auto configured_instance = configured_name_instance.substr(idx + 1, std::string::npos);
         if (name == configured_name) {
-          add_plugin<T>(configured_name, configured_instance);
+          add_plugin_instance<T>(configured_name_instance);
         }
       } else if (name == configured_name_instance) {
-        add_plugin<T>(configured_name_instance, "");
+        add_plugin_instance<T>(configured_name_instance);
       }
     }
   }
 
   template<typename T>
-  void add_plugin(std::string const & name, std::string const & instance) {
+  void add_plugin_instance(std::string const & name) {
     auto const & subtree = config_.subtree(name);
     auto & plugin = *plugins_.emplace_back(new T(subtree));
 
